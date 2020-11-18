4bea7c7387858f8f007873bfd80313fa5d21dd5e
onkarsawant <ops8383@g.rit.edu>
2020-11-18 09:59:03 -0500

    added new api calls for duplicate and deduplicate

--- monolithstarter-svc/src/main/java/com/validity/monolithstarter/rest/HelloController.java
+++ monolithstarter-svc/src/main/java/com/validity/monolithstarter/rest/HelloController.java
@@ -1,10 +1,11 @@
 package com.validity.monolithstarter.rest;
 
+import com.validity.monolithstarter.service.FindDuplicateService;
 import com.validity.monolithstarter.service.HelloService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.inject.Inject;
 
 @RestController
@@ -13,9 +14,25 @@ public class HelloController {
 
     @Inject
     private HelloService helloService;
+    @Inject
+    private FindDuplicateService findDuplicateService;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/hello")
     public String getHelloMessage() {
         return helloService.getHelloMessage();
     }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/duplicates")
+    public String getDuplicates() {
+        return findDuplicateService.getDuplicates().toString();
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/deduplicates")
+    public String getDeduplicates() {
+        return findDuplicateService.getDeduplicates().toString();
+    }
+
 }
